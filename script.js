83cd5c4aa231820874164228201f43258e2d7b91
stefxm <smairenax@gmail.com>
2021-06-24 23:21:51 -0400

    fixed speech functionality

--- script.js
+++ script.js
@@ -3,15 +3,46 @@ const pauseButton = document.getElementById('pause-button')
 const stopButton = document.getElementById('stop-button')
 const textInput = document.getElementById('text')
 const speedInput = document.getElementById('speed')
+let currentCharacter
 
 playButton.addEventListener('click', () => {
     playText(textInput.value)
 })
 
+pauseButton.addEventListener('click', pauseText)
+
+stopButton.addEventListener('click', stopText)
+speedInput.addEventListener('input', () => {
+    stopText()
+    playText(utterance.text.substring(currentCharacter))
+})
+
+const utterance = new SpeechSynthesisUtterance()
+utterance.addEventListener('end', () => {
+    textInput.disabled = false
+})
+utterance.addEventListener('boundary', e => {
+//position of where we are in the string
+    currentCharacter = e.charIndex
+})
+
+
 function playText(text) { // api that pulls the text to speech
-    const utterance = new SpeechSynthesisUtterance(text)
+    if (speechSynthesis.paused && speechSynthesis.speaking) {
+        return speechSynthesis.resume()
+    }
+    if (speechSynthesis.speaking) return
+    utterance.text = text
     utterance.rate = speedInput.value || 1
-    utterance.addEventListener('end')
     textInput.disabled = true
     speechSynthesis.speak(utterance)
+}
+
+function pauseText() {
+    if (speechSynthesis.speaking) speechSynthesis.pause()
+}
+
+function stopText() {
+    speechSynthesis.resume()
+    speechSynthesis.cancel()
 }
