22ea8388d7d11d9f245f1f53a7c016f3f532ba1d
stefxm <smairenax@gmail.com>
2021-06-24 22:59:07 -0400

    setting up speech

---
+++ script.js
@@ -0,0 +1,17 @@
+const playButton = document.getElementById('play-button')
+const pauseButton = document.getElementById('pause-button')
+const stopButton = document.getElementById('stop-button')
+const textInput = document.getElementById('text')
+const speedInput = document.getElementById('speed')
+
+playButton.addEventListener('click', () => {
+    playText(textInput.value)
+})
+
+function playText(text) { // api that pulls the text to speech
+    const utterance = new SpeechSynthesisUtterance(text)
+    utterance.rate = speedInput.value || 1
+    utterance.addEventListener('end')
+    textInput.disabled = true
+    speechSynthesis.speak(utterance)
+}
